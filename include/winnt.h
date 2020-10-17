bad9e123012a3980b479243a5734f06e63f58f39
Alex Henrie <alexhenrie24@gmail.com>
2020-10-17 16:49:39 -0600

    include: Add PROCESS_MITIGATION_PAYLOAD_RESTRICTION_POLICY
    Signed-off-by: Alex Henrie <alexhenrie24@gmail.com>
    ---
    Needed by
    https://hg.mozilla.org/mozilla-central/file/88834ed9b3fc6e6879b1a7f1d4b987ff11ea4b99/mozglue/misc/WindowsProcessMitigations.cpp#l18
    
    Since MinGW copies its headers from Wine now, I assume that Wine is the
    right place to add this struct. Wine will need it eventually anyway to
    implement GetProcessMitigationPolicy.

--- include/winnt.h
+++ include/winnt.h
@@ -6783,6 +6783,30 @@ typedef enum _PROCESS_MITIGATION_POLICY
     MaxProcessMitigationPolicy
 } PROCESS_MITIGATION_POLICY, *PPROCESS_MITIGATION_POLICY;
 
+typedef struct _PROCESS_MITIGATION_PAYLOAD_RESTRICTION_POLICY
+{
+    union
+    {
+        ULONG Flags;
+        struct
+        {
+            ULONG EnableExportAddressFilter:1;
+            ULONG AuditExportAddressFilter:1;
+            ULONG EnableExportAddressFilterPlus:1;
+            ULONG AuditExportAddressFilterPlus:1;
+            ULONG EnableImportAddressFilter:1;
+            ULONG AuditImportAddressFilter:1;
+            ULONG EnableRopStackPivot:1;
+            ULONG AuditRopStackPivot:1;
+            ULONG EnableRopCallerCheck:1;
+            ULONG AuditRopCallerCheck:1;
+            ULONG EnableRopSimExec:1;
+            ULONG AuditRopSimExec:1;
+            ULONG ReservedFlags:20;
+        } DUMMYSTRUCTNAME;
+    } DUMMYUNIONNAME;
+} PROCESS_MITIGATION_PAYLOAD_RESTRICTION_POLICY, *PPROCESS_MITIGATION_PAYLOAD_RESTRICTION_POLICY;
+
 #ifdef _MSC_VER
 
 BOOLEAN _BitScanForward(unsigned long*,unsigned long);
